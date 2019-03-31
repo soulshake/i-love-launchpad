c86c59311ef2c8c7fa69b4f68ff2e68f70ac2679
AJ Bowen <aj@soulshake.net>
2019-03-31 15:15:50 +0200

    with rgb

--- hue.py
+++ hue.py
@@ -3,16 +3,41 @@ import random
 import mido
 import colors
 import time
+import logging
+import os
+
+log_format = "[%(levelname)s] %(filename)s:%(lineno)d %(funcName)s() -> %(message)s"
+log_level = os.environ.get("LOG_LEVEL", "INFO").upper()
+logging.basicConfig(level=log_level, format=log_format)
+
+# /etc/X11/rgb.txt
+
+class Grid(object):
+    def __init__(self):
+        pass
+
+    def clear(self, first_button=11, end=99):
+        for note in range(first_button, end):
+            p.send(mido.Message("note_off", note=note))
+
+    def buttons(self):
+        first_button = 11
+        ret = []
+        for first_button in [11, 21, 31, 41, 51, 61, 71, 81]:
+            for x in range(first_button, first_button+8):
+                ret.append(x)
+        return ret
 
 
+def setup():
+    portnames = mido.get_ioport_names()
+    p = mido.open_ioport(portnames[1])
 
-mido.get_ioport_names()
+    # enter programmer mode
+    p.send(mido.Message("sysex", data=[0, 32, 41, 2, 16, 44, 3]))
+    return p
 
-portnames = mido.get_ioport_names()
-p = mido.open_ioport(portnames[1])
 
-# enter programmer mode
-p.send(mido.Message("sysex", data=[0, 32, 41, 2, 16, 44, 3]))
 """
 LED=0
 for R in [10, 20, 30, 40, 50, 60]:
@@ -39,17 +64,26 @@ def blink(color="YELLOW", count=3):
                 p.send(mido.Message(status, note=note, velocity=color))
             time.sleep(0.3)
 
-def clear(begin, end):
-    for note in range(begin, end):
-        p.send(mido.Message("note_off", note=note))
-
-def display_row(order, offset=0):
-    i = 0
-    for item in order:
-        i += 1
-        if not item:
+def display_row(hues, first_button, offset=0):
+    i = -1
+    for hue in hues:
+        if hue == {"R": 0, "G": 0, "B": 0}:
             continue
-        p.send(mido.Message("note_on", note = i + offset, velocity = order[i-1]))
+        i += 1
+        if not hues:
+            logging.warn("out of hues")
+            return
+
+        LED = first_button + offset + i
+        if LED not in Grid().buttons():
+            i += 1
+            continue
+        R = hue["R"]
+        G = hue["G"]
+        B = hue["B"]
+        msg = mido.Message("sysex", data=[0, 32, 41, 2, 16, 11, LED, R, G, B])
+        logging.debug(msg)
+        p.send(msg)
 
 
 def receive_button_push():
@@ -58,49 +92,97 @@ def receive_button_push():
         if event.type in ["polytouch"]:
             continue
         if event.type in ["control_change"]:
-            print("alerting on control_change event")
+            logging.info("alerting on control_change event")
             return 999
         if event.type == "sysex":
-            print("skipping sysex message: {}".format(event.dict()))
+            logging.info("skipping sysex message: {}".format(event.dict()))
             continue
         if event.velocity == 0:
             continue
+
+        
         return event.note
 
 
-
-
-def play(begin=50, end=58):
-    random_order = [x for x in range(begin, end)]
+def play(hues, first_button=11, end=19):
+    our_range = [x for x in range(first_button, end)]
+    random_order = hues.copy()
     random.shuffle(random_order)
-    fill = [0] * begin
+    fill = [{"R": 0, "G": 0, "B": 0}] * first_button
     random_order = fill + random_order
     new_order = random_order
 
-    display_row(sorted(random_order), offset=20)
-    display_row(random_order)
+    display_row(hues, first_button, offset=20)
+    display_row(random_order, first_button)
 
-    while new_order != sorted(random_order):
+    while new_order != hues:
         guesses = []
-        for x in range(0, 2):
-            guesses.append(receive_button_push())
+        while len(guesses) < 2:
+            guess = receive_button_push()
+            if guess not in our_range:
+                blink(color="RED", count=1)
+                logging.info("discarding invalid guess: {} (not in {})".format(guess, our_range))
+                guesses = []
+                continue
+            guesses.append(guess)
 
         g1, g2 = guesses[0], guesses[1]
-        if g1 - 1 not in random_order or g2 - 1 not in random_order:
-            blink(color="RED", count=1)
-            continue
-        value1 = new_order[g1 - 1]
-        value2 = new_order[g2 - 1]
+        value1 = new_order[g1]
+        value2 = new_order[g2]
         index1 = new_order.index(value1)
         index2 = new_order.index(value2)
 
         new_order[index1] = value2
         new_order[index2] = value1
-        display_row(new_order)
+        logging.info(new_order)
+        logging.info(hues)
+        logging.info("---")
+        display_row(new_order, first_button)
 
-    print("yay!")
+    logging.info("yay!")
     blink(color="GREEN")
 
+def increment_rgb(hue={"R": 64, "G": 64, "B": 64}, intervals={"R": 10, "G": 10, "B": 10}):
+    for k in hue:
+        hue[k] = hue[k] + intervals[k]
+    return hue
 
-clear(11, 99)
-play()
+def fill_scale(begin={"R": random.randint(0, 64), "G": random.randint(0, 64), "B": random.randint(0, 64)}, end={"R": 63, "G": 0, "B": 0}, size=8):
+    ret = [begin]
+
+    new_hues = []
+    for i in range(0, size):
+        new_hue = {}
+        for x in ["R", "G", "B"]:
+            interval = (end[x] - begin[x]) / size
+            new_hue[x] = int(begin[x] + (i * interval))
+        new_hues.append(new_hue)
+
+    return new_hues
+
+
+def generate_scale(begin={"R": 10, "G": 30, "B": 20}, intervals={"R": 9, "G": 4, "B": 9}, ret=[], max=8):
+    # et R, G, B sont dans [0-63]
+    return fill_scale()
+
+    buttons = []
+    for i in range(1, max):
+        button = {}
+        for k in begin:
+            button[k] = 0
+            button[k] += intervals[k]
+            intervals[k] += int((intervals[k] / i))
+        buttons.append(button)
+    return buttons
+
+hues = generate_scale(max=8)
+for hue in hues:
+    print(hue)
+
+p = setup()
+
+g = Grid()
+g.clear()
+
+
+play(hues)
